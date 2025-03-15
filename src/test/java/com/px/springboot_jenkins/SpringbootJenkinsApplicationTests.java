c226672f8be2f3c8a467746ea0055a9b44778087
panxian <pxian20@126.com>
2025-03-15 21:44:46 +0800

    initial

---
+++ src/test/java/com/px/springboot_jenkins/SpringbootJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.px.springboot_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
